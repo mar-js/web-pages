9f15382a0fde2d7f7b6c417f44b925ce96c21902
Martin <maguera37@gmail.com>
2026-06-10 16:42:36 -0300

    feat: updated route names for consistency in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,6 @@ import type { AppRoute } from "./global/interfaces";
 import { NotFoundPage, WebPages } from "./pages";
 
 export const routes: AppRoute[] = [
-  { path: "/", name: "Inicio", element: <WebPages />, exact: true },
-  { path: "*", name: "Inicio", element: <NotFoundPage />, exact: true },
+  { path: "/", name: "Home", element: <WebPages />, exact: true },
+  { path: "*", name: "Not Found", element: <NotFoundPage />, exact: true },
 ];
