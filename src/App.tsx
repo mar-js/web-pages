88d14373015de58a0f91aec4770741e27c83510a
Martin <maguera37@gmail.com>
2026-06-10 16:07:06 -0300

    feat: refactored App component to use Switch and Route for dynamic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import type { JSX } from "react";
-import { WebPages } from "./pages";
+import { Route, Switch } from "wouter";
+import { routes } from "./routes";
 
-export const App = (): JSX.Element => <WebPages />
+export const App = (): JSX.Element => (
+  <Switch>
+    {routes.map(route => <Route key={route.name} path={route.path}>{route.element}</Route> )}
+  </Switch>
+)
