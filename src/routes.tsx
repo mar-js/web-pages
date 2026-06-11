c527f69344695528df0228664087189ccf3dc6e4
Martin <maguera37@gmail.com>
2026-06-11 15:00:26 -0300

    feat: added HuddleLandingPage route to routes configuration

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import type { AppRoute } from "./global/interfaces";
 import { NotFoundPage, WebPages } from "./pages";
+import { HuddleLandingPage } from "./pages/huddle-landing-page";
 
 export const routes: AppRoute[] = [
   { path: "/", name: "Home", element: <WebPages />, exact: true },
+  { path: "/pages/huddle-landing-page", name: "Huddle Landing Page", element: <HuddleLandingPage />, exact: true },
   { path: "*", name: "Not Found", element: <NotFoundPage />, exact: true },
 ];
