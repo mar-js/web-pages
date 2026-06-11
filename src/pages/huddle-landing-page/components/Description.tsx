ec992ca8394f6fb94cffbfa419cf6207c8c1ff8a
Martin <maguera37@gmail.com>
2026-06-11 17:02:21 -0300

    feat: added Description component for Huddle landing page

---
+++ src/pages/huddle-landing-page/components/Description.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export const Description = (): JSX.Element => (
+	<p className="text-white opacity-70">
+		Huddle re-imagines the way we build communities. You have a voice, but so
+		does your audience. Create connections with your users as you engage in
+		genuine discussion.
+	</p>
+);
