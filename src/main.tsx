b75c5eb2503f449f554668c69497d203e7bc5764
Martin <maguera37@gmail.com>
2026-06-11 16:07:11 -0300

    feat: updated global css import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Router } from "wouter";
 import { App } from "./App";
-import "./index.css";
+import "./global/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>
+	<StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</StrictMode>,
 );
