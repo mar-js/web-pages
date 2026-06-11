59dc8a6e47933459a9ac92051f02b6f0c923d480
Martin <maguera37@gmail.com>
2026-06-11 16:46:32 -0300

    feat: added Hero component for Huddle landing page

---
+++ src/pages/huddle-landing-page/components/Hero.tsx
@@ -0,0 +1,6 @@
+import type { JSX } from "react";
+import { Image } from "./Image";
+
+export const Hero = (): JSX.Element => (
+	<Image path="huddle-landing-page/illustration-mockups.svg" alt="Hero" />
+);
