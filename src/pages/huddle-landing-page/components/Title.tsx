9ad509d4fe57ff9753caad701f025c0c399a5bc6
Martin <maguera37@gmail.com>
2026-06-11 16:51:17 -0300

    feat: added Title component for Huddle landing page

---
+++ src/pages/huddle-landing-page/components/Title.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from "react";
+
+export const Title = (): JSX.Element => (
+	<h2 className="text-center md:text-right">
+		Build The Community <br />
+		Your Fans Will Love
+	</h2>
+);
