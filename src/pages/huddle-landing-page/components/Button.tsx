390bb85671902f714467fbe5fe6fe44a6e944973
Martin <maguera37@gmail.com>
2026-06-12 15:17:55 -0300

    feat: added Button component to Huddle landing page

---
+++ src/pages/huddle-landing-page/components/Button.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from "react";
+
+export const Button = (): JSX.Element => (
+	<a
+		href="./pages/huddle-landing-page"
+		target="_self"
+		className="min-w-52 bg-white rounded-3xl py-3 text-sm text-huddle-purple-700 hover:bg-huddle-magenta-400 hover:text-white hover:opacity-80"
+	>
+		Register
+	</a>
+);
