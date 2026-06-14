82a977e5691dcea678c5bef650e229c705be7620
Martin <maguera37@gmail.com>
2026-06-14 15:37:46 -0300

    feat: updated Section component for responsive text alignment

--- src/pages/huddle-landing-page/layouts/Section.tsx
+++ src/pages/huddle-landing-page/layouts/Section.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Section = ({ children }: PropsWithChildren): JSX.Element => (
-	<section className="flex items-center flex-col gap-4 text-center md:text-left px-16 mb-28">
+	<section className="flex items-center flex-col gap-4 text-center lg:text-left px-16 mb-28">
 		{children}
 	</section>
 );
