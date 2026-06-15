de8c1177d78310bbc0e648c0a31fea0904e7b087
Martin <maguera37@gmail.com>
2026-06-15 15:43:47 -0300

    feat: adjusted the spacing and alignment of the text in the Section component

--- src/pages/huddle-landing-page/layouts/Section.tsx
+++ src/pages/huddle-landing-page/layouts/Section.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Section = ({ children }: PropsWithChildren): JSX.Element => (
-	<section className="flex items-center flex-col gap-4 text-center lg:text-left px-16 mb-28">
+	<section className="flex items-center flex-col gap-4 text-center xl:text-left px-12 mb-28">
 		{children}
 	</section>
 );
