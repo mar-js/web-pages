95df560207325f3c02ac91505c9d92e67efbb1cd
Martin <maguera37@gmail.com>
2026-06-16 16:23:36 -0300

    feat: updated Section component width for improved layout on larger screens

--- src/pages/huddle-landing-page/layouts/Section.tsx
+++ src/pages/huddle-landing-page/layouts/Section.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Section = ({ children }: PropsWithChildren): JSX.Element => (
-	<section className="flex items-center flex-col gap-4 text-center px-12 xl:text-left xl:w-[50%] xl:justify-start">
+	<section className="flex items-center flex-col gap-4 text-center xl:items-start xl:text-left xl:w-2/5">
 		{children}
 	</section>
 );
