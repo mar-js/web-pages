c06d19f2ebfe9f09bea0b443f7a084f7c995bb65
Martin <maguera37@gmail.com>
2026-06-16 15:50:27 -0300

    feat: added text-center class for improved button alignment

--- src/pages/huddle-landing-page/components/Button.tsx
+++ src/pages/huddle-landing-page/components/Button.tsx
@@ -4,7 +4,7 @@ export const Button = (): JSX.Element => (
 	<a
 		href="./pages/huddle-landing-page"
 		target="_self"
-		className="min-w-52 bg-white rounded-3xl py-3 text-sm text-huddle-purple-700 hover:bg-huddle-magenta-400 hover:text-white hover:opacity-80"
+		className="min-w-52 bg-white rounded-3xl py-3 text-sm text-huddle-purple-700 text-center hover:bg-huddle-magenta-400 hover:text-white hover:opacity-80"
 	>
 		Register
 	</a>
