e3d111802fae02daa202143114f3c24338a384af
Martin <maguera37@gmail.com>
2026-06-16 15:44:00 -0300

    feat: added className for responsive styling in Hero component

--- src/pages/huddle-landing-page/components/Hero.tsx
+++ src/pages/huddle-landing-page/components/Hero.tsx
@@ -2,5 +2,9 @@ import type { JSX } from "react";
 import { Image } from "./Image";
 
 export const Hero = (): JSX.Element => (
-	<Image path="huddle-landing-page/illustration-mockups.svg" alt="Hero" />
+	<Image
+		path="huddle-landing-page/illustration-mockups.svg"
+		alt="Hero"
+		className="w-full xl:w-200 h-auto object-cover"
+	/>
 );
