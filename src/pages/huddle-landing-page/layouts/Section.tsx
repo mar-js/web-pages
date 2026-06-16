a31044d8268feda1668e5f18da95f0dc35e1727b
Martin <maguera37@gmail.com>
2026-06-16 15:59:09 -0300

    feat: updated Section component layout for improved responsiveness

--- src/pages/huddle-landing-page/layouts/Section.tsx
+++ src/pages/huddle-landing-page/layouts/Section.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Section = ({ children }: PropsWithChildren): JSX.Element => (
-	<section className="flex items-center flex-col gap-4 text-center xl:text-left px-12 mb-28">
+	<section className="flex items-center flex-col gap-4 text-center px-12 xl:text-left xl:w-[50%] xl:justify-start">
 		{children}
 	</section>
 );
