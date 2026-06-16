176a85e1ec83961b85377ba54adf95453f48d4c9
Martin <maguera37@gmail.com>
2026-06-16 16:45:17 -0300

    fix: removed redundant className property in Button component

--- src/pages/huddle-landing-page/components/Button.tsx
+++ src/pages/huddle-landing-page/components/Button.tsx
@@ -4,7 +4,7 @@ export const Button = (): JSX.Element => (
 	<a
 		href="./pages/huddle-landing-page"
 		target="_self"
-		className="min-w-64 bg-white rounded-full py-3 text-sm text-huddle-purple-700 text-center xl:text-2xl xl:py-4 hover:bg-huddle-magenta-400 hover:text-white hover:opacity-80"
+		className="min-w-64 bg-white rounded-full py-3 text-sm text-huddle-purple-700 text-center xl:py-4 hover:bg-huddle-magenta-400 hover:text-white hover:opacity-80"
 	>
 		Register
 	</a>
