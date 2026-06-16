b94c57126c2cb689e9380fc91075e572fad465a2
Martin <maguera37@gmail.com>
2026-06-16 15:49:36 -0300

    feat: added full width and responsive alignment to Icons component

--- src/pages/huddle-landing-page/components/Icons.tsx
+++ src/pages/huddle-landing-page/components/Icons.tsx
@@ -3,7 +3,7 @@ import { ICONS_HUDDLE_LANDING_PAGE } from "../../../constants/data";
 import { Image } from "./Image";
 
 export const Icons = (): JSX.Element => (
-	<ul className="flex items-center justify-center gap-5">
+	<ul className="w-full flex items-center justify-center xl:justify-end gap-5">
 		{ICONS_HUDDLE_LANDING_PAGE.map((icon) => (
 			<li key={icon} className="border border-white rounded-full p-2">
 				<Image className="size-4 invert" path={icon} alt={icon} />
