13c4035af0dbb2b7b45d5a52d3f0eb27dbc26220
Martin <maguera37@gmail.com>
2026-06-16 17:09:00 -0300

    fix: reordering exports in index.tsx for consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,4 @@
-export { WebPages } from "./web-pages"
-export { NotFoundPage } from "./not-found-page"
+export { FyloDarkLandingPage } from "./fylo-dark-landing-page";
+export { HuddleLandingPage } from "./huddle-landing-page";
+export { NotFoundPage } from "./not-found-page";
+export { WebPages } from "./web-pages";
