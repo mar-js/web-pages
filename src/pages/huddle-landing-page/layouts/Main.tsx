0fc7ea263f800324b3cb515d57e020d5c66bf832
Martin <maguera37@gmail.com>
2026-06-16 15:58:55 -0300

    feat: added margin-bottom to Main component for improved layout spacing

--- src/pages/huddle-landing-page/layouts/Main.tsx
+++ src/pages/huddle-landing-page/layouts/Main.tsx
@@ -1,5 +1,5 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Main = ({ children }: PropsWithChildren): JSX.Element => (
-	<main className="flex flex-wrap gap-20">{children}</main>
+	<main className="flex flex-wrap gap-20 mb-28">{children}</main>
 );
