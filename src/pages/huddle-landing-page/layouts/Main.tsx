22202e82fb05f64be98621223e79334c0c7b02ad
Martin <maguera37@gmail.com>
2026-06-16 16:45:31 -0300

    fix: updated layout in Main component to adjust gap and alignment

--- src/pages/huddle-landing-page/layouts/Main.tsx
+++ src/pages/huddle-landing-page/layouts/Main.tsx
@@ -1,5 +1,5 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Main = ({ children }: PropsWithChildren): JSX.Element => (
-	<main className="flex flex-wrap gap-20 mb-28">{children}</main>
+	<main className="flex flex-wrap justify-between gap-5 mb-28">{children}</main>
 );
