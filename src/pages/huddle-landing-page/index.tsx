7d498482f085c15130d80f312d2c44d4c834a9b0
Martin <maguera37@gmail.com>
2026-06-16 16:58:20 -0300

    feat: implemented HuddleLandingPage layout with Header, Main, Section, and Footer components

--- src/pages/huddle-landing-page/index.tsx
+++ src/pages/huddle-landing-page/index.tsx
@@ -1,3 +1,24 @@
 import type { JSX } from "react";
+import { Button, Description, Hero, Icons, Logo, Title } from "./components";
+import { Footer, Header, Main, Section } from "./layouts";
 
-export const HuddleLandingPage = (): JSX.Element => <h1>HuddleLandingPage</h1>
+export const HuddleLandingPage = (): JSX.Element => (
+	<div className="w-full min-h-screen bg-huddle-purple-700 bg-[url('/assets/huddle-landing-page/bg-mobile.svg')] bg-center bg-cover xl:bg-[url('/assets/huddle-landing-page/bg-desktop.svg')]">
+		<div className="layout">
+			<Header>
+				<Logo />
+			</Header>
+			<Main>
+				<Hero />
+				<Section>
+					<Title />
+					<Description />
+					<Button />
+				</Section>
+			</Main>
+			<Footer>
+				<Icons />
+			</Footer>
+		</div>
+	</div>
+);
