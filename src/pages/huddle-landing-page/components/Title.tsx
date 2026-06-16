82712a52f71c9d08255fab51c11289196f70dcda
Martin <maguera37@gmail.com>
2026-06-16 15:58:49 -0300

    feat: added responsive text size to Title component

--- src/pages/huddle-landing-page/components/Title.tsx
+++ src/pages/huddle-landing-page/components/Title.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from "react";
 
 export const Title = (): JSX.Element => (
-	<h2 className="text-white font-semibold text-3xl">
+	<h2 className="text-white font-semibold text-3xl xl:text-4xl">
 		Build The Community <br />
 		Your Fans Will Love
 	</h2>
