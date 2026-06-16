93d65f50c9ba99c5c0539d1a3f47d7437158ecb4
Martin <maguera37@gmail.com>
2026-06-16 17:09:04 -0300

    fix: reorganized imports and routes in routes.tsx for clarity

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,24 @@
 import type { AppRoute } from "./global/interfaces";
-import { NotFoundPage, WebPages } from "./pages";
-import { HuddleLandingPage } from "./pages/huddle-landing-page";
+import {
+	FyloDarkLandingPage,
+	HuddleLandingPage,
+	NotFoundPage,
+	WebPages,
+} from "./pages";
 
 export const routes: AppRoute[] = [
-  { path: "/", name: "Home", element: <WebPages />, exact: true },
-  { path: "/pages/huddle-landing-page", name: "Huddle Landing Page", element: <HuddleLandingPage />, exact: true },
-  { path: "*", name: "Not Found", element: <NotFoundPage />, exact: true },
+	{ path: "/", name: "Home", element: <WebPages />, exact: true },
+	{
+		path: "/pages/huddle-landing-page",
+		name: "Huddle Landing Page",
+		element: <HuddleLandingPage />,
+		exact: true,
+	},
+	{
+		path: "/pages/fylo-dark-landing-page",
+		name: "Fylo Dark Landing Page",
+		element: <FyloDarkLandingPage />,
+		exact: true,
+	},
+	{ path: "*", name: "Not Found", element: <NotFoundPage />, exact: true },
 ];
