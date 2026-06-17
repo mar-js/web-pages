913e456ff41ff7a40f055d6163f8f7e45ac36a46
Martin <maguera37@gmail.com>
2026-06-17 14:05:44 -0300

    feat: added Logo component for Fylo dark landing page

---
+++ src/pages/fylo-dark-landing-page/components/Logo.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from "react";
+import { Image } from "./Image";
+
+export const Logo = (): JSX.Element => (
+	<h1 className="max-w-52">
+		<Image path="fylo-dark-landing-page/logo.svg" alt="Fylo" />
+	</h1>
+);
