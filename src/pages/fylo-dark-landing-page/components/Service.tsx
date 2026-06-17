7e2f909b53d1df55b24f84f1de4c4e1e2a56624b
Martin <maguera37@gmail.com>
2026-06-17 15:40:57 -0300

    feat: added Service component for displaying services on Fylo dark landing page

---
+++ src/pages/fylo-dark-landing-page/components/Service.tsx
@@ -0,0 +1,18 @@
+import type { JSX } from "react";
+import { DATA_SERVICE_FYLO_DARK_LANDING_PAGE } from "../../../constants/data";
+import { Image } from "./Image";
+
+export const Service = (): JSX.Element => (
+	<div className="flex justify-center items-center flex-wrap gap-25 py-25">
+		{DATA_SERVICE_FYLO_DARK_LANDING_PAGE.map((service) => (
+			<article
+				key={service.title}
+				className="flex flex-col justify-center items-center gap-4 text-center text-white"
+			>
+				<Image path={service.img} alt={service.title} className="max-w-15" />
+				<h3 className="text-lg">{service.title}</h3>
+				<p className="text-md opacity-60">{service.description}</p>
+			</article>
+		))}
+	</div>
+);
