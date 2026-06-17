9702ff08ae3d4844ce7fa29398fe5b55644d6736
Martin <maguera37@gmail.com>
2026-06-17 14:20:31 -0300

    feat: added DATA_LINKS constant for navigation items

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -3,3 +3,5 @@ export const ICONS_HUDDLE_LANDING_PAGE: string[] = [
 	"huddle-landing-page/x.svg",
 	"huddle-landing-page/instagram.svg",
 ];
+
+export const DATA_LINKS: string[] = ["Features", "Team", "Sign In"];
