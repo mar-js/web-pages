54ef9b408127aa748db4d0795933a878e06fbbc5
Martin <maguera37@gmail.com>
2026-06-17 15:40:47 -0300

    feat: added data service constants for Fylo dark landing page

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,3 +1,5 @@
+import type { DataServiceFyloDarkLandingPage } from "../global/interfaces";
+
 export const ICONS_HUDDLE_LANDING_PAGE: string[] = [
 	"huddle-landing-page/facebook.svg",
 	"huddle-landing-page/x.svg",
@@ -5,3 +7,31 @@ export const ICONS_HUDDLE_LANDING_PAGE: string[] = [
 ];
 
 export const DATA_LINKS: string[] = ["Features", "Team", "Sign In"];
+
+export const DATA_SERVICE_FYLO_DARK_LANDING_PAGE: DataServiceFyloDarkLandingPage[] =
+	[
+		{
+			img: "fylo-dark-landing-page/icon-access-anywhere.svg",
+			title: "Access your files, anywhere",
+			description:
+				"The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+		},
+		{
+			img: "fylo-dark-landing-page/icon-security.svg",
+			title: "Security you can trust",
+			description:
+				"2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+		},
+		{
+			img: "fylo-dark-landing-page/icon-collaboration.svg",
+			title: "Real-time collaboration",
+			description:
+				"Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+		},
+		{
+			img: "fylo-dark-landing-page/icon-any-file.svg",
+			title: "Store any type of file",
+			description:
+				"Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+		},
+	];
