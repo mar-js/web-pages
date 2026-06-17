cec6190e4cd4eea50fcc60e4d39b6f30c9790a21
Martin <maguera37@gmail.com>
2026-06-17 15:42:52 -0300

    feat: added max-w-105 class to article in Service component for improved layout

--- src/pages/fylo-dark-landing-page/components/Service.tsx
+++ src/pages/fylo-dark-landing-page/components/Service.tsx
@@ -7,7 +7,7 @@ export const Service = (): JSX.Element => (
 		{DATA_SERVICE_FYLO_DARK_LANDING_PAGE.map((service) => (
 			<article
 				key={service.title}
-				className="flex flex-col justify-center items-center gap-4 text-center text-white"
+				className="max-w-105 flex flex-col justify-center items-center gap-4 text-center text-white"
 			>
 				<Image path={service.img} alt={service.title} className="max-w-15" />
 				<h3 className="text-lg">{service.title}</h3>
