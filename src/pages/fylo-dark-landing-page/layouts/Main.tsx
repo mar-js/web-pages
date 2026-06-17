4172ff622f5eea7d31f31a16f61e3ec4e5ddea71
Martin <maguera37@gmail.com>
2026-06-17 15:15:47 -0300

    feat: updated Main component to use layout-container and remove-layout-container classes for consistent styling

--- src/pages/fylo-dark-landing-page/layouts/Main.tsx
+++ src/pages/fylo-dark-landing-page/layouts/Main.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Main = ({ children }: PropsWithChildren): JSX.Element => (
-	<main className="p-10 sm:p-14 md:p-20 remove-layout bg-[url('/assets/fylo-dark-landing-page/bg-curvy-mobile.svg')] bg-bottom bg-no-repeat bg-cover xl:bg-[url('/assets/fylo-dark-landing-page/bg-curvy-desktop.svg')]">
+	<main className="layout-container remove-layout-container bg-[url('/assets/fylo-dark-landing-page/bg-curvy-mobile.svg')] bg-bottom bg-no-repeat bg-cover xl:bg-[url('/assets/fylo-dark-landing-page/bg-curvy-desktop.svg')]">
 		{children}
 	</main>
 );
