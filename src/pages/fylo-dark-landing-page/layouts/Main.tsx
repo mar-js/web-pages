74a12ff5ee33ac56387f595bfedf7f05aa3e89f6
Martin <maguera37@gmail.com>
2026-06-17 15:08:10 -0300

    feat: replaced -m-10, -m-14 and -m-20 with the .remove-layout class in the Main component

--- src/pages/fylo-dark-landing-page/layouts/Main.tsx
+++ src/pages/fylo-dark-landing-page/layouts/Main.tsx
@@ -1,7 +1,7 @@
 import type { JSX, PropsWithChildren } from "react";
 
 export const Main = ({ children }: PropsWithChildren): JSX.Element => (
-	<main className="p-10 sm:p-14 md:p-20 -m-10 sm:-m-14 md:-m-20 bg-[url('/assets/fylo-dark-landing-page/bg-curvy-mobile.svg')] bg-bottom bg-no-repeat bg-cover xl:bg-[url('/assets/fylo-dark-landing-page/bg-curvy-desktop.svg')]">
+	<main className="p-10 sm:p-14 md:p-20 remove-layout bg-[url('/assets/fylo-dark-landing-page/bg-curvy-mobile.svg')] bg-bottom bg-no-repeat bg-cover xl:bg-[url('/assets/fylo-dark-landing-page/bg-curvy-desktop.svg')]">
 		{children}
 	</main>
 );
