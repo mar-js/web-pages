d64e765e63b80cdb9cd18ba6f0041902ec17a6c7
Martin <maguera37@gmail.com>
2026-06-17 14:12:54 -0300

    fix: adjusted max-width of Logo component for better layout

--- src/pages/fylo-dark-landing-page/components/Logo.tsx
+++ src/pages/fylo-dark-landing-page/components/Logo.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "react";
 import { Image } from "./Image";
 
 export const Logo = (): JSX.Element => (
-	<h1 className="max-w-52">
+	<h1 className="max-w-28">
 		<Image path="fylo-dark-landing-page/logo.svg" alt="Fylo" />
 	</h1>
 );
