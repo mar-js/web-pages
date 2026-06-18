c29bf04c3798d45ede82d50409da1cbf8d15ac2e
Martin <maguera37@gmail.com>
2026-06-18 16:23:07 -0300

    feat: added rounded styling to Ceo component and adjusted image class for better presentation

--- src/pages/fylo-dark-landing-page/components/Ceo.tsx
+++ src/pages/fylo-dark-landing-page/components/Ceo.tsx
@@ -5,12 +5,19 @@ import { Image } from "./Image";
 export const Ceo = (): JSX.Element => (
 	<div className="w-full flex flex-wrap justify-between items-center gap-10 text-white py-28">
 		{DATA_CEO_FYLO_DARK_LANDING_PAGE.map((ceo) => (
-			<article key={ceo.img} className="max-w-86 bg-fylo-dark-navy-800 p-8">
+			<article
+				key={ceo.img}
+				className="max-w-86 bg-fylo-dark-navy-800 p-8 rounded-lg"
+			>
 				<p className="opacity-80 text-sm mb-4">{ceo.description}</p>
 				<div className="flex justify-start items-center gap-4">
-					<Image path={ceo.img} alt={ceo.title} className="min-w-5" />
-					<div>
-						<h5 className="text-sm">{ceo.title}</h5>
+					<Image
+						path={ceo.img}
+						alt={ceo.title}
+						className="max-w-8 rounded-full"
+					/>
+					<div className="leading-3">
+						<h5 className="text-[10px]">{ceo.title}</h5>
 						<span className="text-[10px] opacity-80">{ceo.subtitle}</span>
 					</div>
 				</div>
