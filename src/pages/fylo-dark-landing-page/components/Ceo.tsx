ea92767fe9542c7b639cc87fcd5b1e61fecec7fb
Martin <maguera37@gmail.com>
2026-06-18 16:29:15 -0300

    feat: reorganized imports and added conditional rendering for quote image in Ceo component

--- src/pages/fylo-dark-landing-page/components/Ceo.tsx
+++ src/pages/fylo-dark-landing-page/components/Ceo.tsx
@@ -1,14 +1,24 @@
 import type { JSX } from "react";
-import { DATA_CEO_FYLO_DARK_LANDING_PAGE } from "../../../constants/data";
+import {
+	DATA_CEO_FYLO_DARK_LANDING_PAGE,
+	IMG_WORK_QUOTE_FYLO_DARK_LANDING_PAGE,
+} from "../../../constants/data";
 import { Image } from "./Image";
 
 export const Ceo = (): JSX.Element => (
 	<div className="w-full flex flex-wrap justify-center items-center gap-10 text-white py-28">
-		{DATA_CEO_FYLO_DARK_LANDING_PAGE.map((ceo) => (
+		{DATA_CEO_FYLO_DARK_LANDING_PAGE.map((ceo, index) => (
 			<article
 				key={ceo.img}
-				className="max-w-86 bg-fylo-dark-navy-800 p-8 rounded-lg"
+				className="max-w-86 bg-fylo-dark-navy-800 p-8 rounded-lg relative"
 			>
+				{!index && (
+					<Image
+						path={IMG_WORK_QUOTE_FYLO_DARK_LANDING_PAGE}
+						alt="Quote"
+						className="absolute -top-4 left-0 max-w-5"
+					/>
+				)}
 				<p className="opacity-80 text-sm mb-4">{ceo.description}</p>
 				<div className="flex justify-start items-center gap-4">
 					<Image
