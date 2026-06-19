32d6f7c9e22da1f09a0142ed602479fc718b1ba7
Martin <maguera37@gmail.com>
2026-06-19 16:20:07 -0300

    feat: updated icon import to SOCIAL_MEDIA_ICONS for consistency in Huddle landing page

--- src/pages/huddle-landing-page/components/Icons.tsx
+++ src/pages/huddle-landing-page/components/Icons.tsx
@@ -1,10 +1,10 @@
 import type { JSX } from "react";
-import { ICONS_HUDDLE_LANDING_PAGE } from "../../../constants/data";
+import { SOCIAL_MEDIA_ICONS } from "../../../constants/data";
 import { Image } from "./Image";
 
 export const Icons = (): JSX.Element => (
 	<ul className="w-full flex items-center justify-center xl:justify-end gap-5">
-		{ICONS_HUDDLE_LANDING_PAGE.map((icon) => (
+		{SOCIAL_MEDIA_ICONS.map((icon) => (
 			<li key={icon} className="border border-white rounded-full p-2">
 				<Image className="size-4 invert xl:size-6" path={icon} alt={icon} />
 			</li>
