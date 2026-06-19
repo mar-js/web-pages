92dc0c0877992faac8e61d2586a78a50f00401cc
Martin <maguera37@gmail.com>
2026-06-19 16:22:07 -0300

    feat: added Icons component for social media integration in Fylo dark landing page

---
+++ src/pages/fylo-dark-landing-page/components/Icons.tsx
@@ -0,0 +1,13 @@
+import type { JSX } from "react";
+import { SOCIAL_MEDIA_ICONS } from "../../../constants/data";
+import { Image } from "./Image";
+
+export const Icons = (): JSX.Element => (
+	<ul className="w-full flex items-center justify-center xl:justify-end gap-5">
+		{SOCIAL_MEDIA_ICONS.map((icon) => (
+			<li key={icon} className="border border-white rounded-full p-2">
+				<Image className="size-4 invert xl:size-6" path={icon} alt={icon} />
+			</li>
+		))}
+	</ul>
+);
