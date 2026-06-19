28fd490c96e9669836ae3735ea9841fcdb3faa70
Martin <maguera37@gmail.com>
2026-06-19 16:47:10 -0300

    feat: adjusted style class in the Icons component for better alignment

--- src/pages/fylo-dark-landing-page/components/Icons.tsx
+++ src/pages/fylo-dark-landing-page/components/Icons.tsx
@@ -3,7 +3,7 @@ import { SOCIAL_MEDIA_ICONS } from "../../../constants/data";
 import { Image } from "./Image";
 
 export const Icons = (): JSX.Element => (
-	<ul className="w-full flex items-center justify-center xl:justify-end gap-5">
+	<ul className="h-fit flex gap-5">
 		{SOCIAL_MEDIA_ICONS.map((icon) => (
 			<li key={icon} className="border border-white rounded-full p-2">
 				<Image className="size-4 invert xl:size-6" path={icon} alt={icon} />
