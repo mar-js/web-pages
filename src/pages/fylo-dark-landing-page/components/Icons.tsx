d90cd04f3f796d824f47f4fa132d1fa178a7cdf1
Martin <maguera37@gmail.com>
2026-06-28 16:14:55 -0300

    feat: updated Icons component to enhance layout with justify classes

--- src/pages/fylo-dark-landing-page/components/Icons.tsx
+++ src/pages/fylo-dark-landing-page/components/Icons.tsx
@@ -3,7 +3,7 @@ import { SOCIAL_MEDIA_ICONS } from "../../../constants/data";
 import { Image } from "./Image";
 
 export const Icons = (): JSX.Element => (
-	<ul className="h-fit flex gap-5">
+	<ul className="h-fit flex justify-center xl:justify-start gap-5">
 		{SOCIAL_MEDIA_ICONS.map((icon) => (
 			<li key={icon} className="border border-white rounded-full p-2">
 				<Image className="size-5 invert" path={icon} alt={icon} />
