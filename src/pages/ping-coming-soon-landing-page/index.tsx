4f4965352684d626e785d8aba7861a5309d16f53
Martin <maguera37@gmail.com>
2026-07-05 17:15:30 -0300

    feat: added component PingComingSoonLandingPage

---
+++ src/pages/ping-coming-soon-landing-page/index.tsx
@@ -0,0 +1,7 @@
+import type { JSX } from "react";
+
+export const PingComingSoonLandingPage = (): JSX.Element => (
+	<div className="w-full min-h-scree">
+		<div className="layout layout-container">PingComingSoon</div>
+	</div>
+);
