0b58bef1e3d0e1faa3456a0f9c9648338067e2bb
Martin <maguera37@gmail.com>
2026-07-06 17:15:43 -0300

    feat: exported PingComingSoonLandingPage from index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 export { FyloDarkLandingPage } from "./fylo-dark-landing-page";
 export { HuddleLandingPage } from "./huddle-landing-page";
 export { NotFoundPage } from "./not-found-page";
+export { PingComingSoonLandingPage } from "./ping-coming-soon-landing-page";
 export { WebPages } from "./web-pages";
