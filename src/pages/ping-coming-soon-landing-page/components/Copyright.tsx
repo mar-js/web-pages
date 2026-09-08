8749cd039097f6078c9408d7e94d5e5d115af788
Martin <maguera37@gmail.com>
2026-09-08 02:35:54 -0300

    feat: added copyright component for the "ping coming soon" page

---
+++ src/pages/ping-coming-soon-landing-page/components/Copyright.tsx
@@ -0,0 +1,7 @@
+import type { JSX } from "react";
+
+export const Copyright = (): JSX.Element => (
+	<p className="text-ping-coming-soon-gray-400 text-xs">
+		© Copyright Ping. All rights reserved.
+	</p>
+);
