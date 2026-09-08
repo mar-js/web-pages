f774bf93797dc76573610137a5475ffa86bd1c68
Martin <maguera37@gmail.com>
2026-09-08 02:11:37 -0300

    feat: adjusted the font size class in the Subtitle component for better responsiveness

--- src/pages/ping-coming-soon-landing-page/components/Subtitle.tsx
+++ src/pages/ping-coming-soon-landing-page/components/Subtitle.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from "react";
 
 export const Subtitle = (): JSX.Element => (
-	<h2 className="text-ping-coming-soon-gray-400 text-3xl font-normal">
+	<h2 className="text-ping-coming-soon-gray-400 text-xl md:text-3xl font-normal">
 		We are launching
 		<span className="text-ping-coming-soon-blue-950 font-semibold"> soon!</span>
 	</h2>
