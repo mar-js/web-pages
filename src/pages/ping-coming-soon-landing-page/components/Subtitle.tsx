9edb57d41ff2037224461bb015ec03ba2eef05e4
Martin <maguera37@gmail.com>
2026-09-08 01:46:34 -0300

    feat: added Subtitle component for PingComingSoonLandingPage

---
+++ src/pages/ping-coming-soon-landing-page/components/Subtitle.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from "react";
+
+export const Subtitle = (): JSX.Element => (
+	<h2 className="text-ping-coming-soon-gray-400 text-3xl font-normal">
+		We are launching
+		<span className="text-ping-coming-soon-blue-950 font-semibold"> soon!</span>
+	</h2>
+);
