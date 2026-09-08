b9b708fb656117fb2459f2110d69478f9a872c9f
Martin <maguera37@gmail.com>
2026-09-08 02:36:37 -0300

    feat: refactored PingComingSoonLandingPage structure and layout components

--- src/pages/ping-coming-soon-landing-page/index.tsx
+++ src/pages/ping-coming-soon-landing-page/index.tsx
@@ -1,7 +1,30 @@
+import { Image } from "@global/components";
 import type { JSX } from "react";
+import { IMG_HERO_PING_COMING_SOON_LANDING_PAGE } from "../../constants/data";
+import { Copyright, Form, Hero, Icons, Title } from "./components";
+import { Footer, Header, Main, Section } from "./layouts";
 
 export const PingComingSoonLandingPage = (): JSX.Element => (
-	<div className="w-full min-h-scree">
-		<div className="layout layout-container">PingComingSoon</div>
+	<div className="w-full min-h-screen">
+		<div className="layout layout-container">
+			<Header>
+				<Title />
+			</Header>
+			<Main>
+				<Hero />
+				<Form />
+			</Main>
+			<Section>
+				<Image
+					className="w-150"
+					path={IMG_HERO_PING_COMING_SOON_LANDING_PAGE}
+					alt="Hero"
+				/>
+			</Section>
+			<Footer>
+				<Icons />
+				<Copyright />
+			</Footer>
+		</div>
 	</div>
 );
