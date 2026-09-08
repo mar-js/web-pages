b371673a9e844fb623ea9c0ef99ff5f75a994d2d
Martin <maguera37@gmail.com>
2026-09-08 02:14:31 -0300

    feat: adjusted the size of the inputs in the Form component to improve responsiveness

--- src/pages/ping-coming-soon-landing-page/components/Form.tsx
+++ src/pages/ping-coming-soon-landing-page/components/Form.tsx
@@ -3,12 +3,12 @@ import type { JSX } from "react";
 export const Form = (): JSX.Element => (
 	<form className="flex flex-wrap gap-2 items-center justify-center">
 		<input
-			className="w-64 border border-ping-coming-soon-blue-200 text-ping-coming-soon-blue-200 px-6 py-3 rounded-full text-sm shadow"
+			className="w-72 border border-ping-coming-soon-blue-200 text-ping-coming-soon-blue-200 px-6 py-3 rounded-full text-sm shadow"
 			type="email"
 			placeholder="Your email address"
 		/>
 		<button
-			className="w-64 md:w-36	 border border-ping-coming-soon-blue-500 bg-ping-coming-soon-blue-500 text-white px-6 py-3 rounded-full text-sm shadow"
+			className="w-72 md:w-40	 border border-ping-coming-soon-blue-500 bg-ping-coming-soon-blue-500 text-white px-6 py-3 rounded-full text-sm shadow"
 			type="submit"
 		>
 			Notify Me
