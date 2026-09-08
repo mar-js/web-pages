b68746b3b9de333d05abccc8ca5374fdebd90892
Martin <maguera37@gmail.com>
2026-09-08 02:11:47 -0300

    feat: updated Form component layout and input/button styles for improved responsiveness

--- src/pages/ping-coming-soon-landing-page/components/Form.tsx
+++ src/pages/ping-coming-soon-landing-page/components/Form.tsx
@@ -1,14 +1,14 @@
 import type { JSX } from "react";
 
 export const Form = (): JSX.Element => (
-	<form className="flex flex-col gap-2">
+	<form className="flex flex-wrap gap-2 items-center justify-center">
 		<input
-			className="min-w-96 border border-ping-coming-soon-blue-200 text-ping-coming-soon-blue-200 px-6 py-3 rounded-full text-sm shadow"
+			className="w-64 border border-ping-coming-soon-blue-200 text-ping-coming-soon-blue-200 px-6 py-3 rounded-full text-sm shadow"
 			type="email"
 			placeholder="Your email address"
 		/>
 		<button
-			className="min-w-96 border border-ping-coming-soon-blue-500 bg-ping-coming-soon-blue-500 text-white px-6 py-3 rounded-full text-sm shadow"
+			className="w-64 md:w-36	 border border-ping-coming-soon-blue-500 bg-ping-coming-soon-blue-500 text-white px-6 py-3 rounded-full text-sm shadow"
 			type="submit"
 		>
 			Notify Me
