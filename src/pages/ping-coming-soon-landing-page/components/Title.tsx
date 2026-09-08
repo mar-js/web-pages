4bfb8fe1aee01696febbbdf2cb880714c635d3d8
Martin <maguera37@gmail.com>
2026-09-08 02:10:40 -0300

    feat: adjusted Title component font size for better responsiveness

--- src/pages/ping-coming-soon-landing-page/components/Title.tsx
+++ src/pages/ping-coming-soon-landing-page/components/Title.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from "react";
 
 export const Title = (): JSX.Element => (
-	<h2 className="text-black font-bold text-3xl">
+	<h2 className="text-black font-bold text-2xl md:text-3xl">
 		PING<span className="text-ping-coming-soon-blue-500">.</span>
 	</h2>
 );
