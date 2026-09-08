b4f8329d14f0d16f7b888592842aeabf044dc237
Martin <maguera37@gmail.com>
2026-09-08 02:13:09 -0300

    feat: adjusted the font size class in the Hero component to improve responsiveness

--- src/pages/ping-coming-soon-landing-page/components/Hero.tsx
+++ src/pages/ping-coming-soon-landing-page/components/Hero.tsx
@@ -4,7 +4,7 @@ import { Subtitle } from "./Subtitle";
 export const Hero = (): JSX.Element => (
 	<article className="flex flex-col justify-center items-center gap-5">
 		<Subtitle />
-		<h3 className="text-ping-coming-soon-gray-400">
+		<h3 className="text-ping-coming-soon-gray-400 text-xs md:text-lg">
 			Subscribe and get notified
 		</h3>
 	</article>
